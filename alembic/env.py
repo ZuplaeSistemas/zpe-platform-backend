0221849eada3a7510c1e9514e81ad468ca3a8fb8
Maykon D. Granemann <maykondgranemann@gmail.com>
2024-10-13 21:15:35 -0300

    Adjustments in envs

--- alembic/env.py
+++ alembic/env.py
@@ -1,10 +1,25 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config, pool
 
 from alembic import context
 from app.db.base_class import Base
-from app.models import user  # Importe as models aqui
+from app.models import user  # Importe suas models aqui
+
+# Lendo as variáveis de ambiente para a URL do banco
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+# Construindo a URL do banco a partir das variáveis de ambiente
+# Construindo a URL do banco a partir das variáveis de ambiente
+db_url = (
+    f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@"
+    f"{POSTGRES_HOST}/{POSTGRES_DB}"
+)
+
 
 config = context.config  # Configuração do Alembic
 
@@ -17,16 +32,18 @@ target_metadata = Base.metadata
 
 
 def run_migrations_offline():
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
+    """Executa as migrações no modo offline."""
+    context.configure(url=db_url, target_metadata=target_metadata, literal_binds=True)
 
     with context.begin_transaction():
         context.run_migrations()
 
 
 def run_migrations_online():
+    """Executa as migrações no modo online, conectando ao banco de dados."""
     connectable = engine_from_config(
         config.get_section(config.config_ini_section),
+        url=db_url,  # Usar a URL construída a partir das variáveis de ambiente
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
