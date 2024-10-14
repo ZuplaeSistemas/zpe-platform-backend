a24fd105d8827b77c7bbf2ad6a120a68e9eecd8d
Maykon D. Granemann <maykondgranemann@gmail.com>
2024-10-13 22:57:48 -0300

    Adjusment db_connection in session

--- app/db/session.py
+++ app/db/session.py
@@ -1,10 +1,17 @@
+import os
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+POSTGRES_USER = os.getenv("POSTGRES_USER", "zuplae")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "platform2024")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "zpe_platform_db")
+
 SQLALCHEMY_DATABASE_URL = (
-    "postgresql+asyncpg://zuplae:platform2024@localhost:5432/zpe_platform_db"
+    f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@"
+    f"{POSTGRES_HOST}/{POSTGRES_DB}"
 )
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
